8c6dcac8485cc0c07361eaf0774f913140f670eb
JohnrBell <johnrbell@gmail.com>
2014-12-29 14:29:32 -0500

    modified to run on DO box

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,10 +1,16 @@
+# require 'active_record'
+
+# ActiveRecord::Base.establish_connection({
+#   :adapter => "postgresql",
+#   :host => "localhost",
+#   :username => "john",
+#   :database => "inspire"
+#   })
+
+#   ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
-  :adapter => "postgresql",
-  :host => "localhost",
-  :username => "john",
-  :database => "inspire"
-  })
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"]  + '@127.0.0.1/oohfancy')
 
-  ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger = Logger.new(STDOUT)  
