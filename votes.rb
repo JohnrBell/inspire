dd36ee0cf11a21f5a68c792ba54bfb0e3d4ddbae
JohnrBell <johnrbell@gmail.com>
2015-01-05 13:52:32 -0500

    Database transfer attempt

--- votes.rb
+++ votes.rb
@@ -3,7 +3,7 @@ get '/postup/:postid' do 																						#request for post upvote
 	score[0].score = score[0].score + 1
 	score[0].save
 	categoryid = score[0].parent_category_id
-	redirect "/cat/#{categoryid}"
+	redirect back
 end
 
 get '/postdown/:postid' do 																					#request for post downvote
@@ -11,7 +11,7 @@ get '/postdown/:postid' do 																					#request for post downvote
 	score[0].score = score[0].score - 1
 	score[0].save
 	categoryid = score[0].parent_category_id
-	redirect "/cat/#{categoryid}"
+	redirect back
 end
 
 get '/:categoryid/replyup/:replyid' do 															#request for reply upvote
@@ -19,7 +19,7 @@ get '/:categoryid/replyup/:replyid' do 															#request for reply upvote
 	score[0].score = score[0].score + 1
 	score[0].save
 	postid = score[0].parent_post_id
-	redirect "/post/#{postid}"
+	redirect back
 end
 
 get '/:categoryid/replydown/:replyid' do 														#request for reply downvote
@@ -27,18 +27,18 @@ get '/:categoryid/replydown/:replyid' do 														#request for reply downvo
 	score[0].score = score[0].score - 1
 	score[0].save
 	postid = score[0].parent_post_id
-	redirect "/post/#{postid}"
+	redirect back
 end
 
 get '/catup/:catid' do 																						#request for category upvote
 	score = Category.where(id: params[:catid]).entries
 	score[0].score = score[0].score + 1
 	score[0].save
-	redirect "/"
+	redirect back
 end
 get '/catdown/:catid' do 																						#request for category upvote
 	score = Category.where(id: params[:catid]).entries
 	score[0].score = score[0].score - 1
 	score[0].save
-	redirect "/"
+	redirect back
 end
